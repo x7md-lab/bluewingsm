f7f9eaa21373d62ad37f823b663cca059b4419ba
x7md <me@x7md.net>
2024-02-26 14:58:57 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+function App() {
+  return (
+    <>
+      <div>
+        <Button> Test!</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
